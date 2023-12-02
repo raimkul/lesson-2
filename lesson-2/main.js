12854b807ff246edc98d678a7e8c4af4ae3c81de
kolbai <timurlanovkolbai@gmail.com>
2023-12-02 11:57:21 +0600

    fhjdfbjhd

---
+++ lesson-2/main.js
@@ -0,0 +1,40 @@
+// Типы Данных
+// 1 Sprint - сьрока
+
+// ключевые слова var let const
+// переменные - озгормо
+// операторы (=,==,===,-,/,-,>< >- <-)
+var carName = "Fit"
+var laptopName = "Acer"
+var userName = "Nurshoola"
+var applePrice = '100'
+// 2 number
+var applePrice2 = 100
+var batteryMass = 70
+// 3 boolean
+var isSnow = false
+var isSnow2 = true
+// 4 null
+var baha = null
+baha -'Baha'
+// 5 undefined
+// 6 ---
+var id = Sunbol("userid")
+// 7 -- bigint
+var maxKm = 100n
+// 8 object {}, Array[]
+var headermanu = ['Home', 'about', 'Contacts']
+var table = {
+    //key : value 
+    color: "red",
+    height: 2,
+    width: 3,
+}
+
+var user = {
+    name: "isa",
+    ismarried: false,
+    salaty: 50000,
+    age: 20,
+    car: null,
+}
